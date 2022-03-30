fea14cc8557cd31f97a043eb372f7440fb5ad19e
TheFastCreeper <andreasmiller@gmx.ch>
2022-03-30 08:56:18 +0200

    Commit mathjs

--- waitTimer.js
+++ waitTimer.js
@@ -1,12 +1,12 @@
-import { randomInt } from 'mathjs'
+import 'mathjs'
 
 console.log("Verified that file is indeed loading now")
 randomTimer()
 
 function randomTimer() {
-    let randomInt = randomInt();
+    let randomInt = math.random
     let randomInt2 = parseInt(randomInt);
-    let randomIntMillis = randomInt * 1000;
+    let randomIntMillis = randomInt2 * 1000;
     console.log("A random timeout of "+randomInt+" Seconds will occur now!");
     setTimeout(randomTimer, randomIntMillis);
 
