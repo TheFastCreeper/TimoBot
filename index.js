f261fa523ab7917c7bd14a44b3a3aecbd56fa398
TheFastCreeper <andreasmiller@gmx.ch>
2022-03-30 13:47:05 +0200

    Added basic help menu

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import bot from "./bot.js";
 import 'dotenv/config';
+import help from './services/helpMenu.js'
 
 bot.launch().catch(err => console.log(err));
+help().catch(err => console.log(err));
