9751eb3c6ee102f738409a4ad172c87a03b938a4
TheFastCreeper <andreasmiller@gmx.ch>
2022-04-05 14:33:43 +0200

    Fixed bug that prevented stopSpamming with second run of command /spamMessage

--- commands/spamMessage.js
+++ commands/spamMessage.js
@@ -1,21 +1,35 @@
 import bot from "../bot.js";
-import {randomString} from "../index.js";
+import {randomStringGen} from "../services/randomStringGen.js";
+import { randomTimer } from "../services/waitTimer.js"
+
 
 
 const spamMessage = async () => {
+
     bot.command('spamMessage', async (ctx) => {
+        let randomString = randomStringGen();
+        let randomStringCmd = "/"+randomString;
+        let randomTime = randomTimer();
+        console.log(randomStringCmd);
         const spamming = setInterval(async () => {
             await bot.telegram.sendMessage(
-                '897402878',
+                '2055340600',
                 "Type the command /" + randomString + " to stop the bot!"
+
             );
-            await bot.hears(/.*/, (context) => {
-                console.log(context.update.message.text)
-                if (context.update.message.text === "/" + randomString) clearInterval(spamming);
+            randomTime = randomTimer();
+            console.log(randomTime);
+            await bot.hears(randomStringCmd, (context) => {
+                console.log("Context: "+context.update.message.text)
+                if (context.update.message.text.includes(randomString)){
+                    clearInterval(spamming);
+                    console.log("Includes = true");
+                }
                 context.replyWithMarkdown("Spamming terminated")
             });
 
-        }, 500);
+        }, randomTime);
+
     });
 }
 
--- services/waitTimer.js
+++ services/waitTimer.js
@@ -1,8 +1,8 @@
 import {randomInt} from "mathjs";
 
 
-export const randomTimer = (func) => {
-    const randomIntVar = randomInt(200, 1000);
+export const randomTimer = () => {
+    const randomIntVar = randomInt(1000, 5001);
     console.log("A random timeout of " + randomIntVar + " Millis will occur now!");
-    setTimeout(func, randomIntVar);
+    return randomIntVar;
 }
